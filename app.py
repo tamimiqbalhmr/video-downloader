606a26df0b014751aa093df38c49bea349ce066a
tamim <tamimiqbalhmr@gmail.com>
2025-08-10 20:51:14 +0600

    this is app.py files

--- app.py
+++ app.py
@@ -288,5 +288,4 @@ def get_file(client_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-if __name__ === '__main__to__the__file':    
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5000)
